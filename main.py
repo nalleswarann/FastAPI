9b1c4b1ee3e23e903e164d7ba96e55846252d4d5
nalleswaran916@gmail.com <nalleswaran916@gmail.com>
2024-05-23 21:58:58 +0530

    Modified main.py file

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, HTTPException
+from typing import List
+import uvicorn
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-@app.get("/")
-def name():
-    return {"Hello" : "Nalleswaran"}
+# @app.get("/")
+# def name():
+#     return {"Hello" : "Nalleswaran"}
 
-@app.get("/education/{typeid}")
-def education(typeid : int, q : Union[str, None] = None):
-    return {'typeid' : typeid, "q" : q}
+# @app.get("/education/{typeid}")
+# def education(typeid : int, q : Union[str, None] = None):
+#     return {'typeid' : typeid, "q" : q}
 
-@app.get("/about")
-def show_aboutpage():
-    return {'data' : 'about page'}
+# @app.get("/about")
+# def show_aboutpage():
+#     return {'data' : 'about page'}
+
+
+#--------------------------------------------------------------------
+
+#CRUD Operation
+
+class BankCustomer(BaseModel):
+    cus_id: int
+    cus_name: str
+    cus_loginName: str
+    cus_pass: str
+    cus_status: str
+
+customers: List[BankCustomer] = []
+
+@app.get('/customers')
+def show_customers():
+    return customers
+
+@app.post('/customer')
+def create_customer(customer: BankCustomer):
+    customers.append(customer)
+    return customer
+
+@app.put('/customer/{cus_id}')
+def update_customer(cus_id: int, update_customer: BankCustomer):
+    for index, customer in enumerate(customers):
+        if customer.cus_id == cus_id:
+            customers[index] = update_customer
+            return customers
+    raise HTTPException(status_code=404, detail=f"Customer with id {cus_id} not found")
+
+@app.delete('/customer/{cus_id}')
+def delete_customer(cus_id: int):
+  global customers
+  for index, customer in enumerate(customers):
+    if customer.cus_id == cus_id:
+      del customers[index]
+      return customers
+  raise HTTPException(status_code=404, detail=f"Customer with id {cus_id} not found")
+
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="127.0.0.1", port=8000)
