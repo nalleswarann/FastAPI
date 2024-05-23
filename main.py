6bf5c30579feae9faf950f60c4c50a6cc9dbcb1d
nalleswaran916@gmail.com <nalleswaran916@gmail.com>
2024-05-23 19:07:20 +0530

    Modified main.py file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from typing import Union
-from fastapi import uvicorn
 
 app = FastAPI()
 
+@app.get("/")
+def name():
+    return {"Hello" : "Nalleswaran"}
+
+@app.get("/education/{typeid}")
+def education(typeid : int, q : Union[str, None] = None):
+    return {'typeid' : typeid, "q" : q}
+
+@app.get("/about")
+def show_aboutpage():
+    return {'data' : 'about page'}
